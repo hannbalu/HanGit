5a26793c629fa67c704cd7d9f0ee52616c8794c8
hannbalu <273189672@qq.com>
2020-09-16 00:35:33 +0800

    面试题更新

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
-var a = {
-    name: 'bytedance',
-    func: function() {
-      console.log(this.name);
+var num = 5;
+var testLine = /^[0-9\_]+$/;
+var result=0;
+var arr = ['BA','aOWVXARgUbJDG','OPPCSKNS','HFDJEEDA','ABBABBBBAABBBAABAAA'];
+for(var i=0;i<num;i++){
+    var line = arr[i];
+    if(!testLine.test(line) && line.length<11){
+        result++
     }
-  };
-   
-var fun1 = a.func;
-  fun1();
+}
+
+console.log(result);
